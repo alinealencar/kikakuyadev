fc226031866093e7623195e2b45d620076258ab5
Aline <aline.nevesalencar@georgebrown.ca>
2018-03-15 18:46:09 -0400

    Fix date selection in add appt

--- src/main/webapp/resources/js/calendar.js
+++ src/main/webapp/resources/js/calendar.js
@@ -5,13 +5,13 @@ function daysInMonth(month, year) {
 
 $('#year, #month').change(function() {	
   if ($('#year').val().length > 0 && $('#month').val().length > 0) {
-    $('#day').prop('disabled', false);
-    $('#day').find('option').remove();
-
     var daysInSelectedMonth = daysInMonth($('#month').val(), $('#year').val());
 
-    for (var i = 1; i <= daysInSelectedMonth; i++) {
-      $('#day').append($("<option></option>").attr("value", i).text(i));
+    for (var i = 1; i <= 31; i++) {
+    	if(i > daysInSelectedMonth)
+    		$("#day option[value='" + i + "']").remove();
+    	else if($("#day option:contains('" + i + "')").length == 0)
+    		$('#day').append($("<option></option>").attr("value", i).text(i));
     }
 
   } else {
