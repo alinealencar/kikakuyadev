a67945017d87afa0a2dcc64d82e9dc88599e6c49
Aline <aline.nevesalencar@georgebrown.ca>
2018-03-26 17:58:58 -0400

    Update totalbudget column in db

--- kikakuyaDB.sql
+++ kikakuyaDB.sql
@@ -29,7 +29,7 @@ CREATE TABLE Event (
   eventDate   varchar(255), 
   UseruserId  int(10),
   location    varchar(255) NOT NULL,
-  totalBudget decimal(8,2) DEFAULT 0.0, 
+  totalBudget decimal(9,2) DEFAULT 0.0, 
   PRIMARY KEY (eventId));
 CREATE TABLE VendorEvent (
   vendorEventId   int(10) NOT NULL AUTO_INCREMENT,
